348378f77ed2e9321fa75150edd64a50e86f9a2c
sumatokhin <sumatokhin@adv>
2018-01-26 11:28:56 +0300

    delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { usersArr } from './usersArr';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,4 @@ import { usersArr } from './usersArr';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public users: any[] = usersArr;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { UserCardComponent } from './user-card/user-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserCardComponent
   ],
   imports: [
     BrowserModule
