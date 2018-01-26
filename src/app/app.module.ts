d1f50a480bf749433c89361ef82d1394b03d26a9
sumatokhin <sumatokhin@adv>
2018-01-26 11:46:55 +0300

    new component letter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MailBoxComponent } from './mail-box/mail-box.component';
+import { LetterComponent } from './mail-box/letter/letter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MailBoxComponent,
+    LetterComponent,
   ],
   imports: [
     BrowserModule
