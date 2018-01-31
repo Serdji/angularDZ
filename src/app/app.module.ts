4458cc68c20a3922baeede9efb4f1bff36ef53be
sumatokhin <sumatokhin@adv>
2018-01-31 10:51:38 +0300

    домашка 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-
 import { AppComponent } from './app.component';
+import { LogginComponent } from './loggin/loggin.component';
 import { MailBoxComponent } from './mail-box/mail-box.component';
-import { LetterComponent } from './mail-box/letter/letter.component';
+import { LetterComponent } from './letter/letter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LogginComponent,
     MailBoxComponent,
-    LetterComponent,
+    LetterComponent
   ],
   imports: [
     BrowserModule
