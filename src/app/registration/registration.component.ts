f19583bc82e7700281cbff03cba5757b8c67a831
sumatokhin <sumatokhin@adv>
2018-02-06 18:50:31 +0300

    валидация формы

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormBuilder } from '@angular/forms'
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -18,15 +18,28 @@ export class RegistrationComponent implements OnInit {
 
   initForm() {
     this.form = this.fb.group({
-      name: [],
-      surname: [],
-      sex: [],
-      birthday: [],
-      email: []
+      name: ['', [Validators.minLength(2), Validators.required]],
+      surname: ['', [Validators.minLength(2), Validators.required]],
+      sex: ['', [Validators.required]],
+      birthday: ['', [this.checkOnAgeValidator]],
+      email: ['', [Validators.email, Validators.required]]
     });
   }
 
+  checkOnAgeValidator(birthdayData: any): object {
+    const userData = birthdayData.value.split('-')[0];
+    const dateYear = new Date().getFullYear();
+    const age = dateYear - userData;
+    if (age === dateYear) {
+      return { message: 'Вы не выбрали дату'};
+    } else if (age < 18) {
+      return { message: 'Вы слишком молоды'};
+    } else {
+      return null;
+    }
+  }
+
   sendForm() {
-    console.log(this.form.value);
+    console.log(this.form);
   }
 }
