87b0cf66241566d0a2fba08d385789d986398c27
sumatokhin <sumatokhin@adv>
2018-02-07 08:34:44 +0300

    валидация отдельной функцией

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { checkOnAgeValidator } from '../validator/checkOnAgeValidator';
 
 @Component({
   selector: 'app-registration',
@@ -19,24 +19,11 @@ export class RegistrationComponent implements OnInit {
   initForm() {
     this.form = this.fb.group({
       name: ['', [Validators.minLength(2), Validators.required]],
-      surname: ['', [Validators.minLength(2), Validators.required]],
+      surname: ['', [Validators.minLength(2)]],
       sex: ['', [Validators.required]],
-      birthday: ['', [this.checkOnAgeValidator]],
+      birthday: ['', [checkOnAgeValidator]],
       email: ['', [Validators.email, Validators.required]]
     });
-  }
-
-  checkOnAgeValidator(birthdayData: any): object {
-    const userData = birthdayData.value.split('-')[0];
-    const dateYear = new Date().getFullYear();
-    const age = dateYear - userData;
-    if (age === dateYear) {
-      return { message: 'Вы не выбрали дату'};
-    } else if (age < 18) {
-      return { message: 'Вы слишком молоды'};
-    } else {
-      return null;
-    }
   }
 
   sendForm() {
