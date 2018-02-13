9e516fa0e38833e4268494a849fe1a4136867e88
sumatokhin <sumatokhin@adv>
2018-02-13 16:10:56 +0300

    новый компонент отправки письма и переход на него

---
+++ src/app/mail-box/write-letter/write-letter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-write-letter',
+  templateUrl: './write-letter.component.html',
+  styleUrls: ['./write-letter.component.styl']
+})
+export class WriteLetterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
