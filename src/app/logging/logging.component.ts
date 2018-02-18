e5fdd319dcd854d32f74e2a26f1d5148209e902c
sergey <c.c@bk.ru>
2018-02-18 13:08:06 +0300

    Инпут логирования обернут в ангуляровские формы

--- src/app/logging/logging.component.ts
+++ src/app/logging/logging.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { MailBoxService } from '../services/mail-box.service';
 import { SendListService } from '../services/send-list.service';
 import { DeleteListService } from '../services/delete-list.service';
-import { MailService } from '../services/mail.service'
+import { MailService } from '../services/mail.service';
 import { CookieService } from '../services/cookie.service';
+import { emailValidator } from '../validator/emailValidator';
 
 @Component({
   selector: 'app-logging',
@@ -17,14 +19,17 @@ export class LoggingComponent implements OnInit {
   @Output() emailEvent: EventEmitter<string> = new EventEmitter<string>();
   @Output() entranceEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  public mailBoxes: any;
+  formLogging: FormGroup;
 
+  public mailBoxes: any;
   public entrance: boolean = false;
   public isListUser: boolean = false;
   public error: boolean = false;
   public mailBoxArr: string[] = [];
   public email: string;
   public inputValue: any;
+  public entranceInput: string;
+
 
   constructor(
     private _mailBoxService: MailBoxService,
@@ -32,14 +37,19 @@ export class LoggingComponent implements OnInit {
     private _deleteListService: DeleteListService,
     private _mailService: MailService,
     private _cookieService: CookieService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
     ) { }
 
   ngOnInit() {
     this._mailBoxService.mailBoxList.subscribe(mailBoxList => this.mailBoxes = mailBoxList);
+    this.formLogging = this.fb.group({
+      email: ['', [emailValidator]]
+    });
+    this.formLogging.get('email').valueChanges.subscribe(value => this.entranceInput = value);
   }
 
-  entranceMail(value) {
+  entranceMail() {
     this._mailBoxService.mailBoxList.subscribe(mailBoxList => this.mailBoxes = mailBoxList);
     if (this.mailBoxes.length === 0 ) {
       this.isListUser = !this.isListUser;
@@ -47,9 +57,9 @@ export class LoggingComponent implements OnInit {
       for (const mailBox of this.mailBoxes) {
         this.mailBoxArr.push(mailBox.title);
       }
-      this.entrance = this.mailBoxArr.includes(value);
-      this.error = !this.mailBoxArr.includes(value);
-      this.email = value;
+      this.entrance = this.mailBoxArr.includes(this.entranceInput);
+      this.error = !this.mailBoxArr.includes(this.entranceInput);
+      this.email = this.entranceInput;
       this._mailService.mail = this.email;
       if (this.entrance)  {
         this.router.navigate(['mailbox']);
@@ -73,9 +83,9 @@ export class LoggingComponent implements OnInit {
     this.inputValue = ' ';
   }
 
-  onSendMail(e, value) {
+  onSendMail(e) {
     if (e.keyCode === 13) {
-      this.entranceMail(value);
+      this.entranceMail();
     }
   }
 
